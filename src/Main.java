ce878941ed8f08459e76e85c8d5bd3bc4c51b64a
kmaroufi <kmaroufi76@gmail.com>
2016-05-03 18:09:28 +0430

    first!

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.printf("Project was created!");
         System.out.printf("edited from github");
+        System.out.println("completed");
     }
 }
