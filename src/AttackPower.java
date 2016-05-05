3f5afc475f0e4145bb118f1e5c472054bab52330
kmaroufi <kmaroufi76@gmail.com>
2016-05-05 16:31:45 +0430

    add Property and delete AttackPower!

--- src/AttackPower.java
+++
@@ -1,8 +0,0 @@
-/**
- * Created by asus-pc on 5/5/2016.
- */
-public class AttackPower {
-    private double totalAttackPowerEffect;
-    private double totalAttackPowerRatioEffect;
-
-}
---
+++ src/Property.java
@@ -0,0 +1,29 @@
+/**
+ * Created by asus-pc on 5/5/2016.
+ */
+public class Property {
+    private double[] totalEffectOnProperty;
+    private String name;
+
+    private double[] constantProperty;
+    private double[] attackPower;
+    private double[] maximumAttackPowerCoefficient;
+    private double[] maximumHealthCoefficient;
+    private double[] maximumMagicCoefficient;
+    private double[] healthCoefficient;
+    private double[] magicCoefficient;
+    private double[] healthRefillRateCoefficient;
+    private double[] magicRefillRateCoefficient;
+
+    private void calculateProperty() {
+        //TODO
+    }
+
+    public void effect() {
+        //TODO
+    }
+
+    public void removeEffect() {
+        //TODO
+    }
+}
