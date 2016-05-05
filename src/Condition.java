3a312862cb49b7a8016f06e8007511735caf2d91
kmaroufi <kmaroufi76@gmail.com>
2016-05-05 13:31:35 +0430

    add Condition class

---
+++ src/Condition.java
@@ -0,0 +1,73 @@
+/**
+ * Created by asus-pc on 5/5/2016.
+ */
+public class Condition {
+    private double criticalHitChance;
+    private int criticalHitChanceStatus;
+    private double criticalHitDamage;
+    private int criticalHitDamageStatus;
+    private double maximumMagic; // is double true?!
+    private int maximumMagicStatus;
+    private double magicRefillRate;
+    private int magicRefillRateStatus;
+    private double currentMagic;
+    private int currentMagicStatus;
+    private double attackPowerRatio;
+    private int attackPowerRatioStatus;
+    private double attackPowerRatioOnNonTargetedEnemy;
+    private int attackPowerRatioOnNonTargetedEnemyStatus;
+    private double numberOfNonTargetedEnemyEffected;
+    private int numberOfNonTargetedEnemyEffectedStatus;
+    private double energyPoints;
+    private int energyPointsStatus;
+
+    public boolean checkCondition() {
+        //TODO
+        return false;
+    }
+
+    private boolean checkCriticalHitChance() {
+        //TODO
+        return false;
+    }
+
+    private boolean checkCriticalHitDamage() {
+        //ToDO
+        return false;
+    }
+
+    private boolean checkMaximumMagic() {
+        //ToDO
+        return false;
+    }
+
+    private boolean checkMagicRefillRate() {
+        //ToDO
+        return false;
+    }
+
+    private boolean checkCurrentMagic() {
+        //ToDO
+        return false;
+    }
+
+    private boolean checkAttackPowerRatio() {
+        //ToDO
+        return false;
+    }
+
+    private boolean checkAttackPowerRatioOnNonTargetedEnemy() {
+        //ToDO
+        return false;
+    }
+
+    private boolean checkNumberOfNonTargetedEnemyEffected() {
+        //ToDO
+        return false;
+    }
+
+    private boolean checkEnergyPoints() {
+        //ToDO
+        return false;
+    }
+}
