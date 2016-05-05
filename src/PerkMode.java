744697bbede246ad874a80fc3e23ffe1dfe614bc
kmaroufi <kmaroufi76@gmail.com>
2016-05-05 13:06:45 +0430

    add PerkMode class

---
+++ src/PerkMode.java
@@ -0,0 +1,10 @@
+/**
+ * Created by asus-pc on 5/5/2016.
+ */
+public class PerkMode {
+    private Health health;
+    private AttackPower attackPower;
+    private Magic magic;
+    private EnergyPoint energyPoint;
+    private int nonTargetedEnemy; //it isn't clear that this field has int type!
+}
