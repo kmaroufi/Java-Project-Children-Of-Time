ea96ef0fa92e54158b4a3fe21686dc9e4fa773d9
mohsen <mn7697np@gmail.com>
2016-05-06 18:04:16 +0430

    Creating All Getter And Setters For Enemy :D

--- src/Enemy.java
+++ src/Enemy.java
@@ -8,6 +8,29 @@ public class Enemy {
     private Integer attackPower;
     private Double attackPowerRatio;
     //----------------------------------------------------
+    public String getVersion() {
+        return version;
+    }
+
+    public void setVersion(String version) {
+        this.version = version;
+    }
+
+    public Integer getAttackPower() {
+        return attackPower;
+    }
+
+    public void setAttackPower(Integer attackPower) {
+        this.attackPower = attackPower;
+    }
+
+    public Double getAttackPowerRatio() {
+        return attackPowerRatio;
+    }
+
+    public void setAttackPowerRatio(Double attackPowerRatio) {
+        this.attackPowerRatio = attackPowerRatio;
+    }
     //----------------------------------------------------
     protected void doTurn(){
 
