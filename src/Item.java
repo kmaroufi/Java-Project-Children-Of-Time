089608739ec85f421091c224e223b6ae8dd45254
kmaroufi <kmaroufi76@gmail.com>
2016-05-06 13:21:37 +0430

    changing Item class because it is very similar to Skill class.

--- src/Item.java
+++ src/Item.java
@@ -8,10 +8,18 @@ public class Item {
     private String owner; // is this necessary?!
     private ArrayList<String> effectedHero;
     private int size;
+    private boolean isGlobal;
+    private boolean hasEffectedOnEnemy;
+    private int time;
+    private int remainingTime;
     private boolean isInstantlyUsed;
     private int maximumTimeOfUsed;
     private int remainingTimeOfUsed;
     private ArrayList<Property> properties;
+
+    public void isActivated() {
+        //TODO
+    }
 
     public void useItem() {
         //TODO
