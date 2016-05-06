6d49b714448117359a768d6069e595c6673577d6
mohsen <mn7697np@gmail.com>
2016-05-06 18:58:27 +0430

    Enemy Class implemention Removing name from Soldier

--- src/Enemy.java
+++ src/Enemy.java
@@ -2,26 +2,33 @@
  * Created by Future on 5/6/2016.
  */
 import java.util.*;
-public class Enemy {
+public class Enemy extends Soldier{
     public static HashMap<String , ArrayList<Enemy>> listOfEnemies;
     private String version;
-    private Integer attackPower;
     private Double attackPowerRatio;
-    //----------------------------------------------------
+    private String className;
+    private String name;
+
+    //---------------------------------------------------- Constructors
+    public Enemy(){
+    }
+    public Enemy(Integer attackPower , String className, String name){
+        this.setAttackPower(attackPower);
+        this.setClassName(className);
+        this.setName(name);
+    }
+    //---------------------------------------------------- Functions
+    protected void doTurn(){
+
+    }
+
+    //---------------------------------------------------- Getter && Setters
     public String getVersion() {
         return version;
     }
 
     public void setVersion(String version) {
         this.version = version;
-    }
-
-    public Integer getAttackPower() {
-        return attackPower;
-    }
-
-    public void setAttackPower(Integer attackPower) {
-        this.attackPower = attackPower;
     }
 
     public Double getAttackPowerRatio() {
@@ -31,8 +38,22 @@ public class Enemy {
     public void setAttackPowerRatio(Double attackPowerRatio) {
         this.attackPowerRatio = attackPowerRatio;
     }
-    //----------------------------------------------------
-    protected void doTurn(){
 
+    public String getClassName() {
+        return className;
     }
+
+    public void setClassName(String className) {
+        this.className = className;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
 }
