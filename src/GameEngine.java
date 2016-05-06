9924dace3b96fc2dd4d6b3fa3918843cd65aa62f
kmaroufi <kmaroufi76@gmail.com>
2016-05-06 13:29:03 +0430

    add Player and GameEngine class

---
+++ src/GameEngine.java
@@ -0,0 +1,40 @@
+/**
+ * Created by asus-pc on 5/6/2016.
+ */
+public class GameEngine {
+    private Player player;
+
+    public void addNewHeroClass() {
+        //TODO
+    }
+
+    public void addNewHero() {
+        //TODO
+    }
+
+    public void addNewAbility() {
+        //TODO
+    }
+
+    public void addNewItem() {
+        //TODO
+    }
+
+    public void addNewMap() {
+        //TODO
+    }
+
+    public void reset() {
+        //TODO
+    }
+
+    public void doBattle() {
+        //TODO
+    }
+
+    public void play() {
+        //TODO
+    }
+
+
+}
---
+++ src/Player.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+/**
+ * Created by asus-pc on 5/6/2016.
+ */
+public class Player {
+    private ArrayList<Hero> listOfHeros;
+    private int xp;
+    private double money;
+
+    public void doBattle() {
+        //TODO
+    }
+}
