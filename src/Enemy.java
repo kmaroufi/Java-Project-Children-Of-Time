87a28c8533d4ec386ac69f09cec615cc08a0a25f
mohsen <mn7697np@gmail.com>
2016-05-06 15:29:09 +0430

    Empty Class Hero And Enemy

--- src/Enemy.java
+++ src/Enemy.java
@@ -1,5 +1,15 @@
 /**
  * Created by Future on 5/6/2016.
  */
+import java.util.*;
 public class Enemy {
+    public static HashMap<String , ArrayList<Enemy>> listOfEnemies;
+    private String version;
+    private Integer attackPower;
+    private Double attackPowerRatio;
+    //----------------------------------------------------
+    //----------------------------------------------------
+    protected void doTurn(){
+
+    }
 }
--- src/Hero.java
+++ src/Hero.java
@@ -1,5 +1,48 @@
 /**
  * Created by Future on 5/6/2016.
  */
+import java.util.*;
 public class Hero {
+    public static HashMap<String,ArrayList<Hero>> listOfHeros;
+    public static HashMap<String, String> listOfHeroClasses;
+    public static ArrayList<Skill> listOfActiveGlobalSkills;
+    public static ArrayList<Perk> listOfActiveGlobalPerks;
+    private HashMap<String, Item> listOfitems;
+    private Double criticalHitChance;
+    private Double criticalHitChanceRatio;
+    private Double criticalHitDamage;
+    private Double criticalHitDamageRatio;
+    private Integer maximumMagic;
+    private Double magicRefillRate;
+    private Integer currentMagic;
+    private Double attackPowerRatioDuringAttack;		//   :| namusan be hich dardi nemikhore!
+    private Double healthRefillRateRatio;
+    private Double magicRefillRateRatio;
+    private Double energyPointRatio;
+    private Double attackPowerRatioOnNonTargetedEnemy;
+    private Double attackPowerOnNonTargetedEnemy;
+    private Integer numberOfNonTargetedEnemyEffected;
+    private Integer maximumEnergyPoint;
+    private Integer currentEnergyPoint;
+    private Integer inventorySize;
+    private ArrayList<Item> items;
+    private ArrayList<Perk> perks;
+    private ArrayList<Skill> skills;
+
+
+    //------------------------------------------
+    //------------------------------------------
+    public void attack(){
+
+    }
+    public void useSkill(String skillName){
+
+    }
+    public void upgradeAbility(String abilityName){
+
+    }
+    public void useItem(String itemName){
+
+    }
+
 }
