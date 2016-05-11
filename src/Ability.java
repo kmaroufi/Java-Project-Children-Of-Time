4b18fa82f39660fc185b0e43a663bb53126a1b65
kmaroufi <kmaroufi76@gmail.com>
2016-05-11 16:01:07 +0430

    cleaning Main Class. also new concepts in Ability Class!

--- src/Ability.java
+++ src/Ability.java
@@ -1,22 +1,25 @@
-import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Map;
+import java.util.Random;
 
 /**
  * Created by asus-pc on 5/5/2016.
  */
-public abstract class Ability {
+public abstract class Ability<T> {
 
     protected String name;
     protected String ownerName; // in field shayad lazem nabashe, ama be mafhoome code komak mikone.
-    protected ArrayList<Soldier> effectedSoldiers;
-    protected ArrayList<Soldier> relatedSoldiers;
+    protected ArrayList<T> effectedSoldiers;
+    protected ArrayList<T> relatedSoldiers;
     private boolean isGlobal;
     private boolean hasEffectedOnEnemy;
+    private boolean isRandomSoldierSelecting;
+    private int numberOfRelatedSoldiers;
     private int numberOfGrades;
     private int currentGrade;
     private String fieldOfEffecting; // this field can get Hero, Enemy, Ability, item, Shop and ... value.
     private boolean isPermanently;
+    private boolean isEffectDuringAttack;
     private boolean hasCondition;
     private int[] costOfUpgrade;
     public static Map<String, String> listOfAbilities;
@@ -28,7 +31,47 @@ public abstract class Ability {
 
     }
     //-------------------------------------------------------- Functions
+
     abstract public void upgrade();
+
+    private void choosingRelatedSoldiers() {
+        if (this.hasEffectedOnEnemy) {
+            if (this.isRandomSoldierSelecting) {
+                ArrayList<Enemy> enemies = new ArrayList<>();
+                enemies.addAll(GameEngine.listOfEnemies);
+                for (int i = 0; i < this.numberOfRelatedSoldiers; i++) {
+                    Random random = new Random();
+                    int randomIndex = random.nextInt(enemies.size());
+                    this.relatedSoldiers.add((T) enemies.get(randomIndex));
+                    enemies.remove(randomIndex);
+                }
+            }
+            else {
+                ArrayList<String> nameOfEnemies = Display.getAbilityDetailsBeforeUsing();
+                for (String nameOfEnemy: nameOfEnemies) {
+                    this.relatedSoldiers.add((T) Enemy.mapOfEnemies.get(nameOfEnemy));
+                }
+            }
+        }
+        else {
+            if (this.isRandomSoldierSelecting) {
+                ArrayList<Hero> heroes = new ArrayList<>();
+                heroes.addAll(GameEngine.listOfHeroes);
+                for (int i = 0; i < this.numberOfRelatedSoldiers; i++) {
+                    Random random = new Random();
+                    int randomIndex = random.nextInt(heroes.size());
+                    this.relatedSoldiers.add((T) heroes.get(randomIndex));
+                    heroes.remove(randomIndex);
+                }
+            }
+            else {
+                ArrayList<String> nameOfHeroes = Display.getAbilityDetailsBeforeUsing();
+                for (String nameOfHero: nameOfHeroes) {
+                    this.relatedSoldiers.add((T) Hero.mapOfHeroes.get(nameOfHero));
+                }
+            }
+        }
+    }
     //-------------------------------------------------------- Getter And Setters
 
     public boolean isGlobal() {
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import java.lang.reflect.Field;
 import java.lang.reflect.Parameter;
+import java.lang.reflect.Type;
 
 /**
  * Created by asus-pc on 5/3/2016.
@@ -14,21 +15,6 @@ public class Main {
         if(isCustomGame) {
             //getting data
         }
-//        Hero hero = new Hero();
-//        Class cHero = hero.getClass().getSuperclass().getSuperclass();
-//        for (int i = 0; i < cHero.getDeclaredFields().length; i++) {
-//            System.out.println(cHero.getDeclaredFields()[i].getName());
-//        }
-//        System.out.println(cHero.getDeclaredFields());
-//        try {
-//            Field field = cHero.getDeclaredField("attackPower");
-//            field.set(new Property(new PropertyHandler()), 10);
-//
-//            System.out.println(hero.attackPower);
-//        } catch (NoSuchFieldException e) {
-//            e.printStackTrace();
-//        } catch (IllegalAccessException e) {
-//            e.printStackTrace();
-//        }
+
     }
 }
