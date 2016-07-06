9dba0929d5fe56943ca069e095cdb973dad125a0
Future <mn7697np@gmail.com>
2016-07-06 22:47:48 +0430

    Changing KeyInput

--- src/KeyInput.java
+++ src/KeyInput.java
@@ -1,50 +1,22 @@
+import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
 /**
  * Created by Future on 7/6/2016.
  */
-public class KeyInput implements KeyListener {
-    public static boolean RIGHT = false;
-    public static boolean LEFT = false;
-    public static boolean DOWN = false;
-    public static boolean UP = false;
-    public static boolean ENTER = false;
-
-
-    @Override
-    public void keyTyped(KeyEvent e) {
-
-    }
+public class KeyInput extends KeyAdapter {
+    public static final int NUM_OF_KEYS = 256;
+    public static boolean Keys[] = new boolean[NUM_OF_KEYS];
 
     @Override
     public void keyPressed(KeyEvent e) {
-        int codePressed = e.getKeyCode ();
-        switch (codePressed) {
-            case KeyEvent.VK_RIGHT:
-                this.rightClicked ();
-                break;
-            case KeyEvent.VK_LEFT:
-                break;
-            case KeyEvent.VK_UP:
-                break;
-            case KeyEvent.VK_DOWN:
-                break;
-            case KeyEvent.VK_ENTER:
-                break;
-        }
+        Keys[e.getKeyCode()] = true;
     }
 
-    private void rightClicked() {
-        RIGHT = true;
-        LEFT = false;
-        DOWN = false;
-        UP = false;
-        ENTER = false;
-    }
 
     @Override
     public void keyReleased(KeyEvent e) {
-
+        Keys[e.getKeyCode()] = false;
     }
 }
