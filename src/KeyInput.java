30fe535d4442dcea8fcfce258716695edbad3abc
Future <mn7697np@gmail.com>
2016-07-10 14:00:22 +0430

    implementing KeyInput

--- src/KeyInput.java
+++ src/KeyInput.java
@@ -5,9 +5,20 @@ import java.awt.event.KeyListener;
 /**
  * Created by Future on 7/6/2016.
  */
-public class KeyInput extends KeyAdapter {
+public class KeyInput implements KeyListener {
     public static final int NUM_OF_KEYS = 256;
     public static boolean Keys[] = new boolean[NUM_OF_KEYS];
+
+
+    public static boolean wasPressed(int keyCode) {
+        return Keys[keyCode];
+    }
+
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
 
     @Override
     public void keyPressed(KeyEvent e) {
