f91b40e21f2882e607e9990ac40a20a2c99f15d0
Future <mn7697np@gmail.com>
2016-07-12 01:10:19 +0430

    setting Animation Methods

--- src/GUI/Animation.java
+++ src/GUI/Animation.java
@@ -1,5 +1,7 @@
 package GUI;
 
+import java.awt.*;
+
 /**
  * Created by Future on 7/10/2016.
  */
@@ -17,4 +19,30 @@ public class Animation {
         this.numFrames = frames.length;
     }
 
+    private void nextFrame() {
+        currentFrame = frames[index++];
+        if (index >= numFrames)
+            index = 0;
+    }
+
+    public void run() {
+        // Animation is faster the lower speed is
+        count++;
+        if(count > speed){
+            count = 0;
+            nextFrame();
+        }
+
+        // Animation is faster the greater speed is
+//        count += speed;
+//        if (count > 100) { //where 100 is some constant, you choose!
+//            count = 0;
+//            nextFrame();
+//        }
+    }
+
+    public void render(Graphics g, int x, int y){
+        if(currentFrame != null)
+            currentFrame.render(g, x, y);
+    }
 }
